69a3530d4b030522307f73e91b7a010115f9360b
TheWombRaider <bhobbs@users.noreply.github.com>
2018-08-23 12:56:28 -0500

    Hello World Hack #2

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Ben and Andrew
+ * Date: 08/23/2018
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("My name is Ben, and my major is Computer Science.\n");
+
+  return 0;
+}
